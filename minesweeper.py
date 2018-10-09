3678897cc2573cc05b9071049bca49585667ad15
John Serrano <johnserrano404@gmail.com>
2018-10-09 13:39:46 -0700

    slightly smarter solver. it actually gets some of them right!

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,6 @@
 import itertools
 import random
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 
 DEFAULT_X = 15
 DEFAULT_Y = 15
@@ -48,11 +48,10 @@ class MinesweeperBoard(object):
         # Load adjacent. I don't know if there's any reason to do this lazily.
         total = 0
         for dx, dy in list(itertools.product([1, -1, 0], repeat=2)):
-            try:
-                if self.squares[x+dx][y+dy].mine:
-                    total += 1
-            except IndexError:
-                pass
+            if x+dx < 0 or y+dy < 0 or x+dx >= self.x or y+dy >= self.y:
+                continue
+            if self.squares[x+dx][y+dy].mine:
+                total += 1
 
         self.squares[x][y].adjacent = total
 
@@ -63,9 +62,7 @@ class MinesweeperBoard(object):
         return self.squares[x][y]
 
     def flag_square(self, x, y):
-        if not self.squares[x][y].hidden:
-            raise RuntimeError("Cannot flag revealed square")
-
+        assert self.squares[x][y].hidden, "Cannot flag revealed square"
         self.flags.add((x, y))
 
     def pretty_string(self, color=True):
@@ -82,7 +79,7 @@ class MinesweeperBoard(object):
             for y in range(self.y):
                 square = self.squares[x][y]
                 if (x, y) in self.flags:
-                    row.repr.append(flag_repr)
+                    row_repr.append(flag_repr)
                 elif square.hidden:
                     row_repr.append(".")
                 elif square.mine:
@@ -118,16 +115,95 @@ class Solver(object):
         # TODO: keep track of the solution so we can display it and prove we really
         # did solve the puzzle. Should return all our steps.
 
-        while self.get_hidden_coords():
-            self.deduce()
-            self.guess()
-            print self.board.pretty_string()
-            print "=" * self.board.y*2
+        try:
+            num_guesses = 0
+            while self.get_hidden_coords():
+                num_guesses += 1
+                self.guess()
+                self.deduce()
+        except HitMineException:
+            hidden = len(self.get_hidden_coords())
+            percent = (1 - hidden / float(self.board.x * self.board.y)) * 100
+            print "Hit a mine! {}% revealed".format(percent)
+        else:
+            print "Solved!"
+
+        print "used {} guesses".format(num_guesses)
+
+        print self.board.pretty_string()
+
+    def get_bounded_sets(self):
+        # These bounded sets are at the core of our solution. They represent a set
+        # of squares that must contain a number of mines between some minimum and
+        # maximum value. This function discovers those sets and returns a dict
+        # mapping sets of squares to their minimum and maximum values.
+        set_metadata = {} # Tuple of (min_mines, max_mines)
+
+        def get_neighbors_info(x, y):
+            hidden_neighbors = set([])
+            total_flags = 0
+            for dx, dy in list(itertools.product([1, -1, 0], repeat=2)):
+                if (x+dx, y+dy) in self.board.flags:
+                    total_flags += 1
+                    continue
+                if (x+dx < 0
+                    or y+dy < 0
+                    or x+dx >= self.board.x
+                    or y+dy >= self.board.y):
+                    continue
+                if self.board.get_square(x+dx, y+dy) is None:
+                    hidden_neighbors.add((x+dx, y+dy))
+            return frozenset(hidden_neighbors), total_flags
+
+        square_to_sets = defaultdict(list)
+
+        # Generate all sets defined by revealed squares
+        for x, y in itertools.product(range(self.board.x), range(self.board.y)):
+            square = self.board.get_square(x, y)
+            if square is None:
+                continue
+
+            set_coords, num_flags = get_neighbors_info(x, y)
+            for sq in set_coords:
+                square_to_sets[sq].append(set_coords)
+
+            mines_left = square.adjacent - num_flags
+            if set_coords in set_metadata:
+                assert set_metadata[set_coords] == (mines_left, mines_left)
+            else:
+                set_metadata[set_coords] = (mines_left, mines_left)
+
+        # TODO: generate sets by intersection and difference of those
+
+        return set_metadata
 
     def deduce(self):
-        # TODO: deductive solving
-        # Reveal as much of the board based on deductive rules as we can.
-        return
+        # Reveal as much of the board based on deductive rules as we can
+        old_sets = {}
+        new_sets = self.get_bounded_sets()
+
+        def sets_equal(old, new):
+            if len(old) != len(new):
+                return False
+            for k, v in old.iteritems():
+                if k not in new or new[k] != v:
+                    return False
+            return True
+
+        while not sets_equal(old_sets, new_sets):
+            for squares, (min_mines, max_mines) in new_sets.iteritems():
+                if min_mines != max_mines:
+                    continue
+                if max_mines == 0:
+                    for square in squares:
+                        self.board.reveal_square(*square)
+                elif max_mines == len(squares):
+                    for square in squares:
+                        self.board.flag_square(*square)
+
+            old_sets = new_sets
+            new_sets = self.get_bounded_sets()
+
 
     def guess(self):
         # TODO: improve guessing logic.
@@ -137,6 +213,9 @@ class Solver(object):
         # consider mine probability in the future, as well as some other hueristics
         # (whether it is close or far from revealed squares, if it is near an edge)
         
+        coords = tuple(self.get_hidden_coords())
+        if len(coords) == 0:
+            raise Exception("Yeah it's really zero")
         guessed = random.choice(tuple(self.get_hidden_coords()))
         self.board.reveal_square(*guessed)
 
