be97fbbfb99bdb7ef5731de58a05208fe1dfcb7b
John Serrano <johnserrano404@gmail.com>
2018-10-09 11:33:26 -0700

    add HitMineException, flagging, and some other cruft

--- minesweeper.py
+++ minesweeper.py
@@ -7,11 +7,15 @@ DEFAULT_Y = 15
 DEFAULT_NUM_MINES = 40
 
 
+class HitMineException(Exception):
+    pass
+
+
 class Square(object):
-    def __init__(self, hidden=True, mine=False, adjacent=-1):
+    def __init__(self, hidden=True, mine=False):
         self.hidden = hidden
         self.mine = mine
-        self.adjacent = adjacent
+        self.adjacent = -1
 
 
 class MinesweeperBoard(object):
@@ -28,6 +32,8 @@ class MinesweeperBoard(object):
         for line in lines:
             self.squares.append([Square(mine=square=='X') for square in line])
 
+        self.flags = set([])
+
     @classmethod
     def from_file(cls, filename):
         with open(filename, 'r') as f:
@@ -36,6 +42,9 @@ class MinesweeperBoard(object):
     def reveal_square(self, x, y):
         self.squares[x][y].hidden = False
 
+        if self.squares[x][y].mine:
+            raise HitMineException()
+
         # Load adjacent. I don't know if there's any reason to do this lazily.
         total = 0
         for dx, dy in list(itertools.product([1, -1, 0], repeat=2)):
@@ -43,7 +52,6 @@ class MinesweeperBoard(object):
                 if self.squares[x+dx][y+dy].mine:
                     total += 1
             except IndexError:
-                # laziness level over 9000.
                 pass
 
         self.squares[x][y].adjacent = total
@@ -51,19 +59,31 @@ class MinesweeperBoard(object):
     def get_square(self, x, y):
         if self.squares[x][y].hidden:
             return None
+
         return self.squares[x][y]
 
-    def pretty_string(self, color=False):
+    def flag_square(self, x, y):
+        if not self.squares[x][y].hidden:
+            raise RuntimeError("Cannot flag revealed square")
+
+        self.flags.add((x, y))
+
+    def pretty_string(self, color=True):
         if color:
             mine_repr = "\33[31mX\33[0m"
+            flag_repr = "\33[32mF\33[0m"
         else:
             mine_repr = "X"
+            flag_repr = "F"
 
         representation = []
-        for row in self.squares:
+        for x in range(self.x):
             row_repr = []
-            for square in row:
-                if square.hidden:
+            for y in range(self.y):
+                square = self.squares[x][y]
+                if (x, y) in self.flags:
+                    row.repr.append(flag_repr)
+                elif square.hidden:
                     row_repr.append(".")
                 elif square.mine:
                     row_repr.append(mine_repr)
@@ -79,36 +99,30 @@ class MinesweeperBoard(object):
         return "\n".join(representation)
 
     def __str__(self):
-        return self.pretty_string()
+        return self.pretty_string(color=False)
 
 
 class Solver(object):
     def __init__(self, board):
         self.board = board
-        self.mines = set([]) # set of tuple coords
 
     def get_hidden_coords(self):
-        # Really we should keep track of these when we reveal squares, instead of
-        # recreating the set every time we need it. That might be a non-negligible
-        # performance improvement.
+        # Maybe consider not recreating this every time you access it.
         hidden_coords = set([])
         for x, y in itertools.product(range(self.board.x), range(self.board.y)):
             if self.board.get_square(x, y) is None:
                 hidden_coords.add((x, y))
-        return hidden_coords
+        return hidden_coords - self.board.flags
 
     def solve(self):
         # TODO: keep track of the solution so we can display it and prove we really
-        # did solve the puzzle.
+        # did solve the puzzle. Should return all our steps.
 
-        # Exit when a mine is revealed or only mines remain to be revealed
-        # TODO: add things to self.mines
-        while self.mines < self.get_hidden_coords():
-            # TODO: need some better exit conditions
+        while self.get_hidden_coords():
             self.deduce()
             self.guess()
-            print self.board.pretty_string(color=True)
-            print "=" * self.board.y
+            print self.board.pretty_string()
+            print "=" * self.board.y*2
 
     def deduce(self):
         # TODO: deductive solving
@@ -125,9 +139,6 @@ class Solver(object):
         
         guessed = random.choice(tuple(self.get_hidden_coords()))
         self.board.reveal_square(*guessed)
-        if self.board.get_square(*guessed).mine:
-            self.mines.add(guessed)
-        return guessed
 
 
 def create_puzzle(x, y, num_mines):
@@ -153,7 +164,11 @@ def main():
     puzzle = create_puzzle(DEFAULT_X, DEFAULT_Y, DEFAULT_NUM_MINES)
     board = MinesweeperBoard(puzzle)
     solver = Solver(board)
-    solver.solve()
+    try:
+        solver.solve()
+    except HitMineException:
+        print board.pretty_string()
+        print "Hit a mine!"
 
 if __name__ == "__main__":
     main()
